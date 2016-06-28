f79582d53e4a8e2f86a7ff804f6b0ece6bfb56d6
veracl <veraclemens7@googlemail.com>
2016-06-28 18:38:03 +0200

    Create static page 'info'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :ideas
   root :to => redirect('/ideas')
   # The priority is based upon order of creation: first created -> highest priority.
