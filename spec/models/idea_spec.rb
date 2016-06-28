746fb2dfad731ae40acff961224c462f8f54e4b9
veracl <veraclemens7@googlemail.com>
2016-06-28 23:54:47 +0200

    Add third test (unique constraint)

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,18 +1,27 @@
 require "rails_helper"
 
 describe Idea do
-  it "has a title" do # yep, you can totally use 'it'
+  it "has a title" do
     idea = Idea.new
     idea.name = ''
     expect(idea.valid?).to eq(false)
   end
 
-  it "has a unique title" do # yep, you can totally use 'it'
+  it "has a unique title" do
     idea1 = Idea.new
     idea2 = Idea.new
     idea1.name = 'Test'
     idea2.name = 'Test'
     idea2.save
     expect(idea1).not_to be_valid
+  end
+
+  it "has a unique constraint on title" do
+    idea1 = Idea.new
+    idea2 = Idea.new
+    idea1.name = 'Test'
+    idea2.name = 'Test'
+    idea2.save
+    expect{idea1.save(:validate => false)}.to raise_error ActiveRecord::RecordNotUnique
   end
 end
