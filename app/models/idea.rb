e5eb14aed04647d635eca3d5f7b7f6d2b2130e8b
veracl <veraclemens7@googlemail.com>
2016-06-28 22:06:48 +0200

    Add two tests

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,5 @@
 class Idea < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
   mount_uploader :picture, PictureUploader
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -6,4 +6,13 @@ describe Idea do
     idea.name = ''
     expect(idea.valid?).to eq(false)
   end
+
+  it "has a unique title" do # yep, you can totally use 'it'
+    idea1 = Idea.new
+    idea2 = Idea.new
+    idea1.name = 'Test'
+    idea2.name = 'Test'
+    idea2.save
+    expect(idea1).not_to be_valid
+  end
 end
