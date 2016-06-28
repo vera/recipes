a770c6478cd24eb2e1152d28d432d6d209c41c9e
veracl <veraclemens7@googlemail.com>
2016-06-28 23:50:13 +0200

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621160720) do
+ActiveRecord::Schema.define(version: 20160628214214) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20160621160720) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  add_index "ideas", ["name"], name: "index_ideas_on_name", unique: true
 
 end
