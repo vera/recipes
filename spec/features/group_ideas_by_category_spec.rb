036038e147425099cc6429242df7c3d61117fd3d
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-13 01:58:16 +0200

    add feature spec to see ideas grouped by category

---
+++ spec/features/group_ideas_by_category_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Group ideas by category", :type => :feature do
+  let(:category1) { create :category, name: "Wow much category" }
+  let(:category2) { create :category }
+
+  given "given 3 ideas and 2 categories"  do
+    create :idea, category: category1, name: "Very Idea"
+    create :idea, category: category2, name: "So much other category"
+    create :idea, category: category1, name: "Idea Wow"
+  end
+
+  scenario "See categorized ideas" do
+    visit "/ideas"
+
+    grouping_element = find('.category', :text => /Wow much category/)
+    within(grouping_element) do
+      expect(page).to have_text("Very Idea")
+      expect(page).to have_text("Idea Wow")
+      # BUT
+      expect(page).not_to have_text("So much other category")
+    end
+  end
+end
