87797c30d4ba1354f97e1c136faf179f0404a124
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-12 13:42:48 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Category.create!([{
+  name: "Interesting"
+},{
+  name: "Inspirational"
+},{
+  name: "Worth spreading"
+}])
