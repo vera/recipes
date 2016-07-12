4c2d4d0bb0dda62abc45f4ab6ed7f761b67e0910
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-12 13:59:19 +0200

    category must be present for idea

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -3,4 +3,9 @@ require 'support/shared_examples/unique_and_present_name'
 
 describe Idea do
   it_behaves_like 'a model with unique and present name'
+
+  context "without a category" do
+    subject { described_class.new(name: "Aha!", category: nil) }
+    it { is_expected.not_to be_valid }
+  end
 end
