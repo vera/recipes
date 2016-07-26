a870f473f2e757c713e0445127192d5a87f77aad
veracl <veraclemens7@googlemail.com>
2016-07-27 01:25:52 +0200

    Add categories dropdown to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_categories
+
+  def set_categories
+    @categories_dd = Category.order("name ASC")
+  end
 end
