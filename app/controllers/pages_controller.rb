6aa7cdd836db5c182f380c4ad68270402eb7f29b
veracl <veraclemens7@googlemail.com>
2016-07-30 14:45:42 +0200

    New feature: Search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  def search
+    @recipes = Recipe.where("category_id = :cid AND name LIKE :name", cid: params[:category_id], name: "%#{params[:name]}%")
+  end
+
   def info
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/search'
+  post 'pages/search'
+
   get 'pages/info'
 
   #resources :ideas
