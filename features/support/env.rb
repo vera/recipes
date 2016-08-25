c2375dd5c3a265c3cb9f0efb66c7b84ce5b9bbf9
veracl <veraclemens7@googlemail.com>
2016-08-25 18:24:51 +0200

    Fix Travis CI error

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,6 +9,11 @@ require 'cucumber/rails'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 module Cucumber
   module Rails
     class World
