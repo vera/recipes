11b26258a82dc15a13585f9a9839c0520be1a5fc
veracl <veraclemens7@googlemail.com>
2016-11-16 22:34:34 +0100

    Fix factories

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :ingredient do
-    name ""
+    sequence(:name){|n| "Ingredient#{n}" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password "secret"
   end
 end
