1932edfe8574455df3934ba1b2bf745feb79dc04
veracl <veraclemens7@googlemail.com>
2016-11-16 22:53:42 +0100

    Change AccessDenied redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,10 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to new_user_session_path, :alert => exception.message
+    if user_signed_in?
+      redirect_to root_path, alert: exception.message
+    else
+      redirect_to new_user_session_path, alert: exception.message
+    end
   end
 end
